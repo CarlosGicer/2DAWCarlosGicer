26a56a4a8a2284b6a0854caf422b7050bf9ff7ee
CarlosGicer <carlosgicercgc@gmail.com>
2022-09-30 18:02:28 +0200

    Tres primeros ejercicio practica 1

---
+++ tema1/Practica1/Ejercicio2.php
@@ -0,0 +1,12 @@
+<?php
+
+$cadena1 = "Hola a todos ";
+$cadena2 = "mi nombre es Carlos";
+$cadena3 = $cadena1 . $cadena2;
+
+$cadena1 = $cadena1 . $cadena1 . $cadena2;
+
+echo $cadena3 . "</br>";
+echo $cadena1;
+
+?>
